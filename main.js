a2c23c2e0b4f3cef0137fc887e8a1d2be9879080
Maradona Morais <maradona.morais@hotmail.com>
2016-05-18 13:46:11 -0300

    Promise

--- main.js
+++ main.js
@@ -2,29 +2,31 @@ require('es6-promise').polyfill();
 var request = require('request');
 var cheerio = require('cheerio');
 
-exports.getProduct = function(url, callback) {
-    request(url, function(error, response, html) {
-        if(!error) {
-            var $ = cheerio.load(html);
-            var product = {title:"", price:"", thumbnail:""};
+exports.getProduct = function(url) {
+    return new Promise(function(fulfill, reject) {
+        request(url, function(error, response, html) {
+            if(!error) {
+                var $ = cheerio.load(html);
+                var product = {title:"", price:"", thumbnail:""};
 
-            var details = $('.details-product');
-            var mpTitle = details.children('.mp-title');
-            var mpDetails = details.children('.mp-details');
+                var details = $('.details-product');
+                var mpTitle = details.children('.mp-title');
+                var mpDetails = details.children('.mp-details');
 
-            product.title = mpTitle.children('.prodTitle').children('span[itemprop=name]').text();
+                product.title = mpTitle.children('.prodTitle').children('span[itemprop=name]').text();
 
-            product.price = $('.mp-pb-to').attr('data-partner-value');
+                product.price = $('.mp-pb-to').attr('data-partner-value');
 
-            var mpPhotos = $('.mp-photos');
-            product.thumbnail = mpPhotos.children('.carousel').children('.carousel-list').children().first().children('img[itemprop=thumbnail]').attr('data-szimg');
-            if (product.thumbnail == null) {
-                product.thumbnail = mpPhotos.children('.carousel').children('.carousel-list').children().first().children('img[itemprop=thumbnail]').attr('src');
+                var mpPhotos = $('.mp-photos');
+                product.thumbnail = mpPhotos.children('.carousel').children('.carousel-list').children().first().children('img[itemprop=thumbnail]').attr('data-szimg');
+                if (product.thumbnail == null) {
+                    product.thumbnail = mpPhotos.children('.carousel').children('.carousel-list').children().first().children('img[itemprop=thumbnail]').attr('src');
+                }
+
+                fulfill(product);
+            } else {
+                reject({error:"Cannot get product"});
             }
-
-            callback(false, product);
-        } else {
-            callback(true, {error:"Cannot get product"});
-        }
+        });
     });
 };
